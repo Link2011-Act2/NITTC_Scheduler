ec10b15da8c286af680820942665b76298b53bfa
Link.2011 <www.rikus0205.co.jp@gmail.com>
2026-06-14 19:22:44 +0900

    versionName変更

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,7 +33,7 @@ val buildNumberFiles = (
 ).sortedBy { it.relativeTo(rootProject.projectDir).invariantSeparatorsPath }
 
 val appCodeName = "Elena" // トリッカルから取ります
-val appVersionName = "1.0.0-RC0
+val appVersionName = "1.0.0-RC0"
 val buildContentHash = MessageDigest.getInstance("SHA-256").run {
     buildNumberFiles.forEach { file ->
         update(file.relativeTo(rootProject.projectDir).invariantSeparatorsPath.toByteArray())
