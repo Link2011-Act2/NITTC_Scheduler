e9931e300c7f95ece8bf7f2954362202cb70a0dc
Link.2011 <www.rikus0205.co.jp@gmail.com>
2026-07-20 22:39:10 +0900

    リリース用変更

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,7 +33,7 @@ val buildNumberFiles = (
 ).sortedBy { it.relativeTo(rootProject.projectDir).invariantSeparatorsPath }
 
 val appCodeName = "Silphir" // トリッカルから取ります
-val appVersionName = "1.0.0-IntDev_RC6_rev0"
+val appVersionName = "1.0.0-RC6"
 val buildContentHash = MessageDigest.getInstance("SHA-256").run {
     buildNumberFiles.forEach { file ->
         update(file.relativeTo(rootProject.projectDir).invariantSeparatorsPath.toByteArray())
